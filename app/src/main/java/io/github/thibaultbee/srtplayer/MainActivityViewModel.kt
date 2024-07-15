32d305aabf46116e10c19d01c37957980e07f41b
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-07-15 21:29:51 +0200

    chore(*): add license

--- app/src/main/java/io/github/thibaultbee/srtplayer/MainActivityViewModel.kt
+++ app/src/main/java/io/github/thibaultbee/srtplayer/MainActivityViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 Thibault B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.thibaultbee.srtplayer
 
 import android.app.Application
@@ -30,11 +45,6 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     private fun buildPlayer(): Player {
         /**
          * URL format: srt://host:port?streamid=streamid&latency=latency
-         *
-         * Only the following parameters are extracted from the URL query:
-         * - streamid`
-         * - `passphrase`: setting a passphrase with `setCustomCacheKey` method overrides this parameter
-         * - `latency`
          */
         val url = PreferenceManager.getDefaultSharedPreferences(getApplication())
             .getString(
